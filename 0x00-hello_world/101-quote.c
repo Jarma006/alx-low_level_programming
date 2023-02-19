3c51702a24e1313c65afa5b214edd1c83580d992
Jarma006 <jarmamalari@gmail.com>
2023-02-19 06:30:51 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	write(2, "and that place of art is usefull" -Dora Korpar, 2015-10-19\n",59);
+	return (0)
+}
