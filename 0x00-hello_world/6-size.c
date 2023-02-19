1c085e19b1f5f6af268c0dbfb888b03f587d9b7e
Jarma006 <jarmamalari@gmail.com>
2023-02-19 06:09:59 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("size of int Data Types in c =%2d bytes \n", sizeof(short int));
+	Printf("size of Long int Data Types in c =%2d bytes \n", size(long int));
+	printf("size of Float Data Types in c =%2d bytes \n", sizeof(float));
+	printf("size of Double Data Types in c =%2d bytes \n", sizeof(double));
+	printf("size of Long Double Data Types in c =%2d bytes \n", sizeof(long double));
+	printf("size of char Data Types in c =%2d bytes \n", sizeof(char));
+	return (0)
+}
