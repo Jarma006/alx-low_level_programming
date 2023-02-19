2638bf4bc97bce5b01506594d407d9c6c1dbcfcc
Jarma006 <jarmamalari@gmail.com>
2023-02-19 00:03:53 -0800

    5-print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdis.h>
+
+/**
+ * main - Entry point
+ *
+ * Reture: Always 0 (success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle"\n);
+    return (0);
+}
